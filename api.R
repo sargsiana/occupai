097a85d0ca7960f756faf1c1793ddf064182d10f
Anahit Sargsyan <anahit.sargsyan@tum.de>
2025-01-21 11:16:35 +0100

    Update api.R

--- api.R
+++ api.R
@@ -1,21 +1,18 @@
-# Load plumber library
 library(plumber)
 
 #* @post /greet
 #* @param req The incoming request
 #* @serializer unboxedJSON
 function(req) {
-  # Parse the JSON input
   input_data <- jsonlite::fromJSON(req$postBody)
   
-  # Check for the 'name' field
+  # Validate input
   if (!"name" %in% names(input_data)) {
     return(list(error = "Input JSON must include a 'name' field."))
   }
   
-  # Create a response
   name <- input_data$name
-  response <- list(message = paste("Hello,", name, "! Welcome to the API."))
+  response <- list(message = paste("Hello,", name, "! Welcome to Railway!"))
   
   return(response)
 }
