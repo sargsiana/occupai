11d27541761e08041106e111346e3890d0d8a15f
Anahit Sargsyan <anahit.sargsyan@tum.de>
2025-01-21 10:59:11 +0100

    Update api.R

--- api.R
+++ api.R
@@ -1,4 +1,7 @@
 library(plumber)
+pr <- plumber::plumb("api.R")
+pr$run(host = "0.0.0.0", port = as.numeric(Sys.getenv("PORT")))
+
 
 # Define a function to process the survey data
 #* @post /process_survey
