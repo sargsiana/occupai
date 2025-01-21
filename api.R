29315174afe25e8edb2ba44b3a98a1dcd8152605
Anahit Sargsyan <anahit.sargsyan@tum.de>
2025-01-21 11:20:23 +0100

    Update api.R

--- api.R
+++ api.R
@@ -1,4 +1,7 @@
 library(plumber)
+library(plumber)
+pr <- plumber::plumb("api.R")
+pr$run(host = "0.0.0.0", port = as.numeric(Sys.getenv("PORT")))
 
 #* @post /greet
 #* @param req The incoming request
